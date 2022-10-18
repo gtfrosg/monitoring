dab836dd7ea1c8777a5f7b4888f0b2017646fbbe
gtfrosg <gtfrosg@usp.br>
2022-10-18 14:59:39 -0300

    Criar Sala Pró-Aluno Sociais
    Criação de rota, controller e view para simulação da sala da pró-aluno da ciências sociais.

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\SociaisController;
 
 Route::get('/', [IndexController::class,'index']);
+
+Route::get('/sociais', [SociaisController::class,'index']);
